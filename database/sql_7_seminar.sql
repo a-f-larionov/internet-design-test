ae16102ebe94f741f4b91860b639b50dc8fb746a
aflar <a.f.larionov@gmail.com>
2018-03-22 13:27:07 +0300

    fix sql

--- database/sql_7_seminar.sql
+++ database/sql_7_seminar.sql
@@ -5,15 +5,16 @@ CREATE TABLE seminars (
   `begin_date` DATETIME NOT NULL,
   `end_date`   DATETIME NOT NULL,
   `city_id`    INT(11)  NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 )
   ENGINE = innoDB
   DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `cities` (
-  `city_id` INT(11)     NOT NULL AUTO_INCREMENT,
-  `name`    VARCHAR(32) NOT NULL
+  `id` INT(11)     NOT NULL AUTO_INCREMENT,
+  `name`    VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`)
 )
   ENGINE = innoDB
   DEFAULT CHARSET = utf8;
@@ -21,7 +22,8 @@ CREATE TABLE `cities` (
 
 CREATE TABLE `participants` (
   `id`   INT(11)     NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(32) NOT NULL
+  `name` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`)
 )
   ENGINE = innoDB
   DEFAULT CHARSET = utf8;
